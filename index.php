5b7c484df128da9c78b3405d355fa9e4a39f4039
Princess413 <35474633+Princess413@users.noreply.github.com>
2019-01-17 13:47:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <title></title>
 </head>
 <body>
-<img src="classdiagram.png"/>
-<p><b>First step</b><i>is to register to the site or app, on this diagram it is shown as the adopter table is connected to the adoptee
+<img src="class diagram.png"/>
+<p><b>First step</b><i> is to register to the site or app, on this diagram it is shown as the adopter table is connected to the adoptee
 table(child) every detail needed is shown that will be kept on the database for the records. The adoptee table is connected to the
 orphanage table in order to locate the specific orphanage where the adoptee belongs. Then on the adopt table i did consider the generalization
 relationship because it needs all the information regarding the adopter, adoptee and the orphanage. The admin will be considered as the midman
