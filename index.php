2dd6e49d39f26330bb5e08acbf39b99b16dae023
Princess413 <35474633+Princess413@users.noreply.github.com>
2019-01-23 13:38:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,9 @@
 </head>
 <body>
 <img src="classdia.jpg">
+  <p>This Class Diagram means gathering all the information needed who uses the application.
+  The adopter acts as the user that's in search for an adoptee. Admin manages those informations, user connects with the orphanage
+  that will display all the basic information of each adoptee. Adoption, records all the details regarding when and who did the adoption
+  while admin takes the action on updating the details, records and information.</p>
 </body>
 </html>
